24ba9ecd1c0975054cb5299cca9d405500e2197c
YoshijiFujiwara <yfbiology2@gmail.com>
2021-04-03 22:54:09 +0900

    save list of cells

--- packages/local-client/src/components/cell-list.tsx
+++ packages/local-client/src/components/cell-list.tsx
@@ -1,14 +1,25 @@
 import "./cell-list.css";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import { Cell } from "../state";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
+import { useActions } from "../hooks/use-actions";
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id])
   );
+
+  const { fetchCells, saveCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+  }, []);
+
+  useEffect(() => {
+    saveCells();
+  }, []);
 
   const renderedCells = cells.map((cell: Cell) => (
     <Fragment key={cell.id}>
